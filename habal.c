aa02287d1579469a57b2a36d37cefd98e471308e
moharafat <mohamedamr0001@gmail.com>
2024-01-18 10:19:22 +0200

    s

---
+++ habal.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: arguments num.
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	FILE *file;
+	int i, count = 0;
+	char buffer[50];
+	instruction_t sps[] = {
+		{'push', push}, {'pop', pop}, {'display', display}, {'exit', exit}
+	}
+	for (i = 0; i <= 4; i++)
+	{
+		if (s == sps[i].spec)
+		{
+			count = count + sps[i].f(argv);
+			break;
+		}
+		else if (i == 6 && != sps[i].spec)
+		{
+			/* code */
+		}
+	}
+	if (argc != 2)
+	{
+		dprintf(2, "USAGE: monty fil\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], 'r');
+	if (file == NULL)
+	{
+		printf("Error: Can't open file <file>");
+		exit(EXIT_FAILURE);
+	}
+	if (file != NULL)
+	{
+		while (fgets(buffer, sizeof(buffer), file) != NULL)
+		{
+		printf("%s", buffer);
+		}
+		fclose(file);
+	}
+	else
+	{
+		printf("USAGE: monty file");
+		exit(EXIT_FAILURE);
+	}
+	return (0);
+}
+
