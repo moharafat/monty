941b363bf32a16bac3417f79ba9071d0077b583a
moharafat <mohamedamr0001@gmail.com>
2024-01-20 22:14:23 +0200

    س

--- main.c
+++ main.c
@@ -10,12 +10,10 @@ int main(int argc, char **argv)
 	char buffer[50];
 	FILE *file;
 	stack_t *head;
-	char *delim = " \n", *token;
-	int i, count = 0;
+	char *delim = " \n", *token, *yaret;
+	int i, k = 0, count = 0;
 	instruction_t sps[] = {
-		{"push", push}, {"pop", pop}, {"display", display}
-};
-
+		{"push", push}, {"pop", pop}, {"display", display}};
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty fil\n");
@@ -31,15 +29,17 @@ int main(int argc, char **argv)
 	{
 		while (fgets(buffer, sizeof(buffer), file) != NULL)
 		{
-			get_command(buffer, argv);
-			printf("buffer is %s\n", buffer);
-
+			token = strtok(buffer, delim);
+			while (token != NULL)
+			{
+				printf("buffer is%s\n", buffer);
+				token = strtok(NULL, delim);
+			}
 			for (i = 0; i < sizeof(sps) / sizeof(sps[0]); i++)
 			{
 				if (strcmp(buffer, sps[i].opcode) == 0)
 				{
 					sps[i].f(&head, 0);
-					printf("here it is\n");
 					break;
 				}
 			}
--- push.c
+++ push.c
@@ -8,7 +8,8 @@ void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_node;
 
-	if (top == LIMIT)
+	printf("samo3aleko\n");
+	if (top == LIMIT - 1)
 	{
 		fprintf(stderr, "Error: Stack overflow\n");
 		exit(EXIT_FAILURE);
@@ -19,14 +20,16 @@ void push(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-
+	top++;
+	printf("can u see me?\n");
 	new_node->n = line_number;
 	new_node->next = *stack;
 	new_node->prev = NULL;
-
+	printf("%d\n", new_node->n);
 	if (*stack != NULL)
+	{
 		(*stack)->prev = new_node;
-
+	}
 	*stack = new_node;
-	top++;
+	printf("tmam_keda\n");
 }
