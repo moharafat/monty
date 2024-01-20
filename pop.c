25289b15391bc5dde6a3c36fa892e1f373a6e8f0
moharafat <mohamedamr0001@gmail.com>
2024-01-20 12:10:29 +0200

    s

--- pop.c
+++ pop.c
@@ -1,5 +1,25 @@
 #include "monty.h"
+/**
+ * pop - removes top element
+ * @stack: stack
+ * @line_number: line number
+*/
 void pop(stack_t **stack, unsigned int line_number)
 {
+	stack_t *current;
 
+	if (*stack == NULL)
+	{
+		printf("STACK IS EMPTY");
+	}
+	current = *stack;
+	*stack = current->next;
+	if (*stack)
+	{
+		(*stack)->prev = NULL;
+	}
+
+	current->next = NULL;
+	current->prev = NULL;
+	free(current);
 }
